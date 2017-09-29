0ed0adf7ada8477763ee0c178b528b0de8dda1cb
Carles Escrig Royo <esroyo@gmail.com>
2017-09-29 18:35:35 +0200

    Try develop deploy
    Signed-off-by: Carles Escrig Royo <esroyo@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    expoteca.com
+                    dev.expoteca.com
                 </div>
 
                 <div class="links">
